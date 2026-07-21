a6f032909cd57e1354c4f8f39479a4dc3facf2ff
Pablo Cesar Corrales Padilla <devpablo.corrales@gmail.com>
2026-07-21 00:42:57 -0600

    feat: organize DTO files into folders and separate data to handle User Creation

--- src/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ src/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9c5e24378cbb21807373699bd91ec48ccbf95b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68c9f5254d4bc0b0ba48046cdfd7f9332d776596")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
