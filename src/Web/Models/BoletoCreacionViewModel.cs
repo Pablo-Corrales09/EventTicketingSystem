c1ba650a84c5efb06b7e0669d8ef3ce8ce8e6286
Pablo Cesar Corrales Padilla <devpablo.corrales@gmail.com>
2026-08-16 16:11:45 -0600

    feat(Views): Standardize visual presentation across view

--- src/Web/Models/BoletoCreacionViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Web.Models;
-
-public class BoletoCreacionViewModel
-{
-    public int IdEventoLocalidad { get; set; }
-    public int IdUsuario { get; set; }
-    public int IdMedioPago { get; set; }
-}
